93cdb9fa59f41cb0307bbc89eda3f35f16b3694e
viviandosreis <40150927+viviandosreis@users.noreply.github.com>
2021-06-29 23:46:48 -0300

    Create Topico 01 - aula 02.py

---
+++ Topico 01 - aula 02.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Tópico 01 - Aula 02
+
+# In[3]:
+
+
+import numpy as np
+
+#np.random.randint(menorValor,maiorValor,proporção da tabela)
+np.random.randint(1,10,[2,2])
+#array([[7, 1],[8, 5]])
+
+np.random.rand(2,2)
+#array([[0.87193794, 0.49075618],[0.82258484, 0.70771006]])
+
